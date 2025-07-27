06548076513b495c948a567602ca75af165db808
shussain894 <shahhussain1@me.com>
2025-07-27 16:42:52 +0100

    changes

--- routes/check.js
+++ routes/check.js
@@ -4,9 +4,7 @@ const { getVisaRules } = require('../models/visaService');
 
 router.get('/', async (req, res) => {
   try {
-    console.log('Query params:', req.query); // see if any filters passed
     const results = await getVisaRules(req.query);
-    console.log('Results from DB:', results); // see what comes back
     res.json(results);
   } catch (error) {
     console.error(error);
